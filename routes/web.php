445e392f9085c64fbc9846b25fcd70a997cbb6a0
sofiacottone <cottonesofia@outlook.it>
2024-06-07 16:00:51 +0200

    feat: add products route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,15 +20,12 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware((['auth', 'verified']))
     ->name('admin.')
     ->prefix('admin')
     ->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('products', ProductController::class);
     });
 
 Route::middleware('auth')->group(function () {
