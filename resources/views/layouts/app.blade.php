e166241e6496dba3fc55d5979b65ed52e0a9b758
sofiacottone <cottonesofia@outlook.it>
2024-06-07 16:24:18 +0200

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 <body>
     <div id="app">
 
-
+        // prova
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
