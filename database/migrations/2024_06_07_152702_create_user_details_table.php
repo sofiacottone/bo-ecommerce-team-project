9f4a4865f5571458102b07801452e19b017744b9
sofiacottone <cottonesofia@outlook.it>
2024-06-07 18:48:52 +0200

    fix: minor changes

---
+++ database/migrations/2024_06_07_152702_create_user_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('level', 50)->default('basic');
+            $table->string('address', 250)->nullable();
+            $table->string('country', 60)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
